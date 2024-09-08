62e035f493c585bbcf436814d33daec8e3d75bdc
dcamorganda <dcamorganda@gmail.com>
2024-09-09 00:00:27 +0800

    fourth commit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Theme toggle functionality
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
+const themeIcon = document.getElementById('theme-icon');
 
 themeToggle.addEventListener('click', () => {
     body.classList.toggle('light-mode');
-    themeToggle.innerText = body.classList.contains('light-mode') 
-        ? 'Dark Mode' 
-        : 'Light Mode';
+    themeIcon.src = body.classList.contains('light-mode') 
+        ? 'images/moon-icon.svg' 
+        : 'images/sun-icon.svg';
 });
